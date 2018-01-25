3a48d6433dd08555f9ef870bb859af1b4a4869b5
Max Antinone <mantinone@gmail.com>
2018-01-24 17:28:19 -0800

    Playing a basic note

--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
+const audioContext = new AudioContext()
+const oscillator = audioContext.createOscillator()
+oscillator.frequency.value = 300
+oscillator.connect(audioContext.destination)
+//oscillator.start(0)
+
 document.addEventListener( "DOMContentLoaded", function(event) {
   let soundButton = document.getElementById('playNote')
   soundButton.addEventListener('click', playSound )
