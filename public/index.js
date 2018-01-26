46d6d7b53006ba1678006c8b23d784f623a60f86
Max Antinone <mantinone@gmail.com>
2018-01-25 17:51:37 -0800

    Plays a basic triplet with a button

--- public/index.js
+++ public/index.js
@@ -1,14 +1,41 @@
+
 const audioContext = new AudioContext()
-const oscillator = audioContext.createOscillator()
-oscillator.frequency.value = 300
-oscillator.connect(audioContext.destination)
-//oscillator.start(0)
+const oscillatorC = audioContext.createOscillator()
+const oscillatorE = audioContext.createOscillator()
+const oscillatorG = audioContext.createOscillator()
+
+const vol = audioContext.createGain()
+let noise = false
+
+oscillatorC.frequency.value = 261.626
+oscillatorE.frequency.value = 329.628
+oscillatorG.frequency.value = 391.995
+
+oscillatorC.start(0)
+oscillatorE.start(0)
+oscillatorG.start(0)
+
+oscillatorC.connect(vol)
+oscillatorE.connect(vol)
+oscillatorG.connect(vol)
+
+vol.connect(audioContext.destination)
+
+vol.gain.value = 0
+
 
 document.addEventListener( "DOMContentLoaded", function(event) {
   let soundButton = document.getElementById('playNote')
   soundButton.addEventListener('click', playSound )
 })
 
-const playSound = () => {
-  console.log('Hello World');
+const playSound = ( event ) => {
+  console.log(event.target);
+  if(noise){
+    noise = false
+    vol.gain.setTargetAtTime( 0, audioContext.currentTime, 0.1)
+  } else {
+    noise = true
+    vol.gain.setTargetAtTime( 1, audioContext.currentTime, 0.02)
+  }
 }
